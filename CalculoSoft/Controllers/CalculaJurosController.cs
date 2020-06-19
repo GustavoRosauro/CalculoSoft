90204309e4b3306d0c7c0fabed196384ec9064c8
GustavoRosauro <gustavosrosauro@gmail.com>
2020-06-19 20:12:47 -0300

    'finalizadoUrl'

--- CalculoSoft/Controllers/CalculaJurosController.cs
+++ CalculoSoft/Controllers/CalculaJurosController.cs
@@ -24,5 +24,19 @@ namespace CalculoSoft.Controllers
             //retorna calculo Valor Inicial * (1 + juros)^tempo
             return await SoftRule.RetornaCalculo(valorinicial, meses);
         }
+        /// <summary>
+        /// Retorna HTML para clicar e redirecionar para o GIT
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("[action]")]
+        public ContentResult RetornaUrl()
+        {
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                StatusCode = (int)System.Net.HttpStatusCode.OK,
+                Content = "<html><body><h1><a href='https://github.com/GustavoRosauro/CalculoSoft/tree/master/CalculoSoft'>Clique aqui para ir para o git</a></h1></body></html>"
+            };
+        }
     }
 }

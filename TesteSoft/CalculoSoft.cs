83483c78d6703dacbef45cc3977771133f7ce240
GustavoRosauro <gustavosrosauro@gmail.com>
2020-06-21 12:51:43 -0300

    ajustado teste unitários

--- TesteSoft/CalculoSoft.cs
+++ TesteSoft/CalculoSoft.cs
@@ -11,8 +11,8 @@ namespace TesteSoft
         [Test]
         public void ValidaCalculo()
         {
-            double calculo =  SoftRule.RetornaCalculo(100, 5,"", true, 0.01).Result;
-            Assert.AreEqual(calculo,105.10);
+            string calculo =  SoftRule.RetornaCalculo(100, 5,"", true, 0.01).Result;
+            Assert.AreEqual(calculo,"105,10");
         }
     }
 }
